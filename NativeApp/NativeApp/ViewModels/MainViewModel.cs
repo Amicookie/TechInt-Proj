6eeedf7f475a26391e7f12f1c668938182f1ff56
Iza Borek <izaborek53@gmail.com>
2018-11-13 23:59:12 +0100

    Notepad_local

---
+++ NativeApp/NativeApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NativeApp.Models;
+
+namespace NativeApp.ViewModels
+{
+    public class MainViewModel
+    {
+        private DocumentModel _document;
+        public FileViewModel File { get; set; }
+
+        public MainViewModel()
+        {
+            _document = new DocumentModel();
+
+            File = new FileViewModel(_document);
+        }
+    }
+}
