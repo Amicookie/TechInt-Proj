5fbaee3123a7510e20cc586f00c2cee3d8803d9b
Amicookie <varrioue@gmail.com>
2019-01-18 12:01:56 +0100

    Delete unnecessary old stuff

--- Angular-TI-v1-FrontEnd/src/app/user.ts
+++
@@ -1,5 +0,0 @@
-export interface IUser {
-    user_id: number,
-    user_login: string,
-    user_password: string
-}
--- Projekt-TI1/app/views/user.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-user_view = Blueprint('user_view', __name__, template_folder='templates')
